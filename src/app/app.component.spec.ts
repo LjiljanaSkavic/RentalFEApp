1c8134becb2a69a9b71b5cb036411ca90f1dbc85
Ljiljana Skavic <ljiljana.skavic@hotmail.com>
2025-03-01 15:00:27 +0100

    Init

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,13 +17,13 @@ describe('AppComponent', () => {
   it(`should have as title 'rental-fe-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('rental-fe-app');
+    expect(app.title).toEqual('RentalFEApp');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('rental-fe-app app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('RentalFEApp app is running!');
   });
 });
