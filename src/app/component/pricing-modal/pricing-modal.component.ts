bf43cff5f0045a2adec7b8587ac189b433f9f414
Ljiljana Skavic <ljiljana.skavic@hotmail.com>
2025-04-17 23:05:30 +0200

    Add edit pricing modal

--- src/app/component/pricing-modal/pricing-modal.component.ts
+++ src/app/component/pricing-modal/pricing-modal.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { Pricing } from "../../model/Pricing";
+import { Subscription } from "rxjs";
+import { PricingService } from "../../services/pricing.service";
+import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material/dialog";
+
+export interface PricingModalData {
+    pricing: Pricing;
+}
 
 @Component({
-  selector: 'app-pricing-modal',
-  templateUrl: './pricing-modal.component.html',
-  styleUrls: ['./pricing-modal.component.scss']
+    selector: 'app-pricing-modal',
+    templateUrl: './pricing-modal.component.html',
+    styleUrls: ['./pricing-modal.component.scss']
 })
 export class PricingModalComponent {
+    pricingForm: FormGroup = {} as FormGroup;
+    editedPricing: Pricing = {} as Pricing;
+    subscriptions = new Subscription();
 
+    constructor(private _pricingService: PricingService,
+                private dialogRef: MatDialogRef<PricingModalComponent>,
+                @Inject(MAT_DIALOG_DATA) public data: PricingModalData) {
+    }
+
+    ngOnInit(): void {
+        this.buildPricingForm();
+    }
+
+    buildPricingForm(): void {
+        this.pricingForm = new FormGroup({
+            pricePerDay: new FormControl(this.data.pricing.pricePerDay, [Validators.required, Validators.min(0)])
+        });
+    }
+
+    onDialogClose(): void {
+        this.preparePricing();
+    }
+
+    preparePricing(): void {
+        this.editedPricing = {
+            ...this.data.pricing,
+            pricePerDay: this.pricingForm.get('pricePerDay')?.value as number
+        };
+        this.subscriptions.add(
+            this._pricingService.update(this.editedPricing.id, this.editedPricing).subscribe(res => {
+                this.dialogRef.close(res);
+            })
+        );
+    }
+
+    ngOnDestroy(): void {
+        this.subscriptions.unsubscribe();
+    }
 }
